526483d7b849db4a3eee8e01c46a18dc817b2ba5
Pippin Barr <pippin.barr@gmail.com>
2020-08-26 11:20:31 -0400

    Drawing experiment: added color to my cone

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -11,13 +11,25 @@ Here is a description of this template p5 project.
 function setup() {
   createCanvas(500, 500);
 
-  background(255, 138, 1);
+  background(191, 255, 199);
 
   ellipseMode(CORNER);
+
+  noStroke();
+
+  fill(127, 0, 200, 100);
   ellipse(250, 250, 100, 100);
+
+  fill(137, 0, 210, 100);
   ellipse(250, 250, 80, 80);
+
+  fill(147, 0, 220, 100);
   ellipse(250, 250, 60, 60);
+
+  fill(157, 0, 230, 100);
   ellipse(250, 250, 40, 40);
+
+  fill(167, 0, 240, 100);
   ellipse(250, 250, 20, 20);
 }
 
