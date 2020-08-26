767ad19b4bf21d9cf6526f9748a98607e919a551
Pippin Barr <pippin.barr@gmail.com>
2020-08-26 11:02:26 -0400

    Drawing experiment: Drew a cone

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -9,7 +9,16 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
 
+  background(255, 138, 1);
+
+  ellipseMode(CORNER);
+  ellipse(250, 250, 100, 100);
+  ellipse(250, 250, 80, 80);
+  ellipse(250, 250, 60, 60);
+  ellipse(250, 250, 40, 40);
+  ellipse(250, 250, 20, 20);
 }
 
 // draw()
