2d462842e8058740bcf1dadc085fb432c3c8dba5
Pippin Barr <pippin.barr@gmail.com>
2020-08-27 10:37:38 -0400

    Activity 2: Drew my alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -9,7 +9,34 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(640, 480);
 
+  background(255, 100, 100);
+  noStroke();
+
+  // Draw the body
+  fill(127);
+  ellipse(320, 480, 300, 200);
+
+  // Draw the head
+  fill(100);
+  ellipse(320, 240, 250, 400);
+
+  // Draw the eyes
+  fill(0);
+  ellipse(250, 240, 80, 250);
+  ellipse(390, 240, 80, 250);
+
+  // Draw the nostrils
+  fill(0);
+  ellipse(300, 350, 10, 10);
+  ellipse(340, 350, 10, 10);
+
+  // Draw the mouth
+  stroke(200, 0, 0);
+  strokeWeight(5);
+  rectMode(CENTER);
+  rect(320, 390, 100, 25);
 }
 
 // draw()
