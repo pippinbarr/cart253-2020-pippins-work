2dee6fc1df946b6aa14e08142a76da7e5b6573e5
Pippin Barr <pippin.barr@gmail.com>
2020-09-18 15:18:47 -0400

    Learned about Booleans!

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,10 +1,4 @@
-let backgroundShade = 0;
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  speed: 1
-}
+let displayCircle = false;
 
 function setup() {
   createCanvas(500,500);
@@ -12,15 +6,15 @@ function setup() {
 
 
 function draw() {
-  background(backgroundShade);
 
-  circle.x = circle.x + circle.speed;
+  background(0);
 
-  fill(255,255,255);
-
-  if (!(circle.x < width/3)) {
-    fill(255,0,0);
+  if (mouseIsPressed) {
+    displayCircle = true;
   }
 
-  ellipse(circle.x,circle.y,circle.size);
+  if (displayCircle) {
+    ellipse(250,250,100,100);
+  }
+
 }
