5b89d4f6ed36d3e4db8b087c6d0a8fb13ee71d7c
Pippin Barr <pippin.barr@gmail.com>
2020-09-18 14:36:03 -0400

    Learned about basic conditionals!

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,20 +1,36 @@
-/**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 5
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function setup() {
+  createCanvas(500,500);
+}
 
+
+function draw() {
+  background(backgroundShade);
+
+  circle.x = circle.x + circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0) {
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height/2) {
+    fill(255,0,0);
+  }
+
+  if (mouseY > height/2) {
+    fill(0,0,255);
+  }
+
+  ellipse(circle.x,circle.y,circle.size);
 }
