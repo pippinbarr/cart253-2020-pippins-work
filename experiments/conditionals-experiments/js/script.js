33327410297da32ea35f976967e8003b62831e2a
Pippin Barr <pippin.barr@gmail.com>
2020-09-21 11:00:10 -0400

    Learned about loops! Learned about loops! Learned about loops! Learned about loops! Learned about loops! Learned about loops! Learned about loops!

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,20 +1,34 @@
-let displayCircle = false;
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segmentSize: 50
+}
 
 function setup() {
   createCanvas(500,500);
 }
 
-
 function draw() {
-
   background(0);
+  noStroke();
+  fill(100,200,100);
 
-  if (mouseIsPressed) {
-    displayCircle = true;
-  }
+  // let x = caterpillar.x;
+  // let numSegments = 10;
+  // let segmentsDrawn = 0;
+  //
+  // while (segmentsDrawn < numSegments) {
+  //   ellipse(x,caterpillar.y,caterpillar.segmentSize);
+  //   x = x + 40;
+  //   segmentsDrawn++;
+  // }
 
-  if (displayCircle) {
-    ellipse(250,250,100,100);
+  let x = caterpillar.x;
+  let numSegments = 10;
+
+  for (let i = 0; i < numSegments; i++) {
+    ellipse(x,caterpillar.y,caterpillar.segmentSize);
+    x = x + 40;
   }
 
 }
