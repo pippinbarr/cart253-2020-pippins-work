dd3f8611aaaf737cdc75c60a18f299d2837c3375
Pippin Barr <pippin.barr@gmail.com>
2020-09-21 11:49:11 -0400

    Learned about movement!

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,13 +1,14 @@
-let bg = {
-  r: 0,
-  g: 0,
-  b: 0
-}
 
 let circle = {
   x: 250,
   y: 250,
-  size: 100
+  size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 5
 }
 
 
@@ -16,13 +17,29 @@ function setup() {
 }
 
 function draw() {
-  background(bg.r,bg.g,bg.b);
+  background(0);
+
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  }
+  else {
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  }
+  else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx = circle.vx + circle.ax;
+  circle.vx = constrain(circle.vx,-circle.maxSpeed,circle.maxSpeed);
+  circle.vy = circle.vy + circle.ay;
+  circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
 
   ellipse(circle.x,circle.y,circle.size);
-}
-
-function mousePressed() {
-  bg.r = random(0,255);
-  bg.g = random(0,255);
-  bg.b = random(0,255);
 }
