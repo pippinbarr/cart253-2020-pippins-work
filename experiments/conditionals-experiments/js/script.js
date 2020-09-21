7eeafa42c671d1984c7b9e9b1a5e18aefff9ba75
Pippin Barr <pippin.barr@gmail.com>
2020-09-21 15:47:00 -0400

    Learned about push, pop, and transformations

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,45 +1,23 @@
 
-let circle = {
-  x: 250,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  ax: 0,
-  ay: 0,
-  acceleration: 0.25,
-  maxSpeed: 5
-}
-
+let angle = 0;
+let rectScale = 0;
 
 function setup() {
   createCanvas(500,500);
 }
 
 function draw() {
-  background(0);
+  background(127);
 
-  if (mouseX < circle.x) {
-    circle.ax = -circle.acceleration;
-  }
-  else {
-    circle.ax = circle.acceleration;
-  }
+  push();
+  fill(255,0,0);
+  rectMode(CENTER);
+  translate(width/2,height/2);
+  rotate(angle);
+  scale(rectScale);
+  rect(0,0,100,100);
+  pop();
 
-  if (mouseY < circle.y) {
-    circle.ay = -circle.acceleration;
-  }
-  else {
-    circle.ay = circle.acceleration;
-  }
-
-  circle.vx = circle.vx + circle.ax;
-  circle.vx = constrain(circle.vx,-circle.maxSpeed,circle.maxSpeed);
-  circle.vy = circle.vy + circle.ay;
-  circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
-
-  circle.x = circle.x + circle.vx;
-  circle.y = circle.y + circle.vy;
-
-  ellipse(circle.x,circle.y,circle.size);
+  angle = angle + 0.01;
+  rectScale = rectScale + 0.01;
 }
