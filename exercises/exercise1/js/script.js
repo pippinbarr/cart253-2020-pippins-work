daac3d46aaa70ee988ea2709dbec6a8047f9c58f
Pippin Barr <pippin.barr@gmail.com>
2020-09-21 09:49:13 -0400

    Created placeholder project for submission example.

---
+++ exercises/exercise1/js/script.js
@@ -0,0 +1,28 @@
+/**************************************************
+Placeholder Project
+Pippin Barr
+
+This is a project I can display when I create example links
+to show how to submit things!
+**************************************************/
+
+// setup()
+//
+// Make a canvas!
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+// draw()
+//
+// Display some text!
+function draw() {
+  background(0);
+  fill(255);
+  stroke(255, 0, 0);
+  strokeWeight(100);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text("Placeholder project loves you very much.", width / 2, height / 2);
+}
