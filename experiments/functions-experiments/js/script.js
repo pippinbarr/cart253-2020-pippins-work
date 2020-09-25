4f9fbac255647223ba53da0d845a78c9a3700a0c
Pippin Barr <pippin.barr@gmail.com>
2020-09-25 10:37:32 -0400

    Learned about functions

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,7 +1,9 @@
 let circle = {
-  x: 250,
+  x: 0,
   y: 250,
   size: 100,
+  vx: 1,
+  vy: 0
 };
 
 function setup() {
@@ -11,18 +13,34 @@ function setup() {
 function draw() {
   background(0);
 
-  let mouseIsLeft = undefined;
+  move();
+  wrap();
+  display();
+}
 
-  if (mouseX >= width/2) {
-    console.log("Mouse is to the right...");
-    mouseIsLeft = false;
+function move() {
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+}
+
+function wrap() {
+  if (circle.x > width) {
+    reset();
   }
-  else {
-    console.log("Mouse is to the left...");
-    mouseIsLeft = true;
-  }
+}
 
-  //
-
+function display() {
+  fill(255,0,0);
   ellipse(circle.x,circle.y,circle.size);
+}
+
+function reset() {
+  circle.x = 0;
+  circle.vx = circle.vx + 2;
+  circle.vy = circle.vy - 0.25;
+  circle.size = circle.size + 5;
+}
+
+function mousePressed() {
+  reset();
 }
