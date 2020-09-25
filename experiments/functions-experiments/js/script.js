fd80e6ee73ef39f97902a8d01807dda084d583af
Pippin Barr <pippin.barr@gmail.com>
2020-09-25 10:03:31 -0400

    Functions: started the learning journey!

---
+++ experiments/functions-experiments/js/script.js
@@ -0,0 +1,28 @@
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+};
+
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  background(0);
+
+  let mouseIsLeft = undefined;
+
+  if (mouseX >= width/2) {
+    console.log("Mouse is to the right...");
+    mouseIsLeft = false;
+  }
+  else {
+    console.log("Mouse is to the left...");
+    mouseIsLeft = true;
+  }
+
+  //
+
+  ellipse(circle.x,circle.y,circle.size);
+}
