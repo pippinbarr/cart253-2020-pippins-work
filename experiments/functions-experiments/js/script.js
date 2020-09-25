b352f4a5e9baf3d9153864de2a89f1ca8ea39a57
Pippin Barr <pippin.barr@gmail.com>
2020-09-25 11:43:09 -0400

    Learned about return values!

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,23 +1,53 @@
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0
+}
 
 function setup() {
   createCanvas(500,500);
+  reset();
 }
 
 function draw() {
   background(0);
 
-  parallels(100,100,5,1,100,1);
-  parallels(50,50,10,2,20,10);
-  parallels(200,200,15,7,3,20);
-  parallels(312,257,20,0.5,300,2);
+  move();
+  checkOffscreen();
+  display();
 }
 
-function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing) {
-  for (let i = 0; i < numLines; i++) {
-    noStroke();
-    fill(255);
-    rectMode(CENTER);
-    rect(x,y,lineWidth,lineHeight);
-    x = x + lineSpacing;
+function move() {
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+}
+
+function checkOffscreen() {
+  let offscreen = circleIsOffScreen();
+
+  if (offscreen) {
+    reset();
   }
+}
+
+function circleIsOffScreen() {
+  if (circle.x < 0 || circle.x > width || circle.y < 0 || circle.y > height) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function display() {
+  ellipse(circle.x,circle.y,circle.size);
+}
+
+function reset() {
+  circle.x = 250,
+  circle.y = 250;
+  circle.vx = random(-10,10);
+  circle.vy = random(-10,10);
 }
