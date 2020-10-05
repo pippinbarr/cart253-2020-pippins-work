de0db92d6a0e7e56e25d84c4810a52afac5b8395
Pippin Barr <pippin.barr@gmail.com>
2020-10-05 15:42:49 -0400

    Conditionals: Other uses of while loops!

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -1,28 +1,38 @@
 let circle = {
+  x: undefined,
+  y: undefined,
+  size: 100
+};
+
+let dangerZone = {
   x: 250,
   y: 250,
-  size: 100,
+  size: 150
 };
 
 function setup() {
   createCanvas(500,500);
+
+  circle.x = random(0,width);
+  circle.y = random(0,height);
+
+  let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+  while (d < circle.size/2 + dangerZone.size/2) {
+    circle.x = random(0,width);
+    circle.y = random(0,height);
+    d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+  }
 }
 
 function draw() {
   background(0);
 
-  let mouseIsLeft = undefined;
+  // Danger zone
+  noFill();
+  stroke(255,0,0);
+  ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
 
-  if (mouseX >= width/2) {
-    console.log("Mouse is to the right...");
-    mouseIsLeft = false;
-  }
-  else {
-    console.log("Mouse is to the left...");
-    mouseIsLeft = true;
-  }
-
-  //
-
+  fill(255);
+  noStroke();
   ellipse(circle.x,circle.y,circle.size);
 }
