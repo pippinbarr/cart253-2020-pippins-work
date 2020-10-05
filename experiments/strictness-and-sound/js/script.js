ac222e80a9ac7900091f432fd2148c1aeb93377a
Pippin Barr <pippin.barr@gmail.com>
2020-10-05 16:13:44 -0400

    Strictness and sound: learned about basic sound!

---
+++ experiments/strictness-and-sound/js/script.js
@@ -0,0 +1,28 @@
+
+let music;
+
+function preload() {
+  music = loadSound(`assets/sounds/bark.wav`);
+}
+
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  background(0);
+}
+
+function mousePressed() {
+  tryMusic();
+}
+
+function keyPressed() {
+  tryMusic();
+}
+
+function tryMusic() {
+  if (!music.isPlaying()) {
+    music.loop();
+  }
+}
