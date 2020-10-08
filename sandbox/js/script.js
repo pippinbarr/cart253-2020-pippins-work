ac0efad9cc920472987a59486e1a31b439ad749f
Pippin Barr <pippin.barr@gmail.com>
2020-10-08 11:27:30 -0400

    Arrays: fortune teller

--- sandbox/js/script.js
+++ sandbox/js/script.js
@@ -1,74 +1,45 @@
 "use strict";
 
-// Our school of fish
-let school = [];
-let schoolSize = 1;
+// Our array of fortunes, each of which is a string
+// Note that we still use square brackets around the array,
+// but now we list the elements the array should start with
+// separated by commas.
+// As here, we can put each element on a separate line for clarity.
+let fortunes = [
+  `It's not looking great.`,
+  `You will trip over an apple today.`,
+  `Beware of over-friendly cats.`,
+  `Bank error in your favor, collect $200.`,
+  `Start your Korean skincare regime.`,
+  `You will feel better than 20 years ago.`,
+  `David Lynch will call you on your birthday.`,
+  `Happiness is just around the corner.`,
+  `You will make it look easy today.`,
+  `Your future is cloudy.`
+];
 
+// We need a variable to store the chosen fortune so we can
+// display it in draw()
+let chosenFortune = `I am looking into your soul...`;
+
+// setup() gets basic styling ready
 function setup() {
   createCanvas(600, 600);
-
-  for (let i = 0; i < schoolSize; i++) {
-    school[i] = createFish(random(0, width), random(0, height));
-  }
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 10
-  };
-  return fish;
-}
-
-// draw()
-// Moves and displays our fish
+// draw() displays the current fortune
 function draw() {
   background(0);
-
-  for (let i = 0; i < school.length; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
-  }
+  text(chosenFortune, width / 2, height / 2);
 }
 
-
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
-  }
-
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
-
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
-}
-
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-}
-
+// mousePressed() chooses a random fortune from the fortunes array
 function mousePressed() {
-  let fish = createFish(mouseX, mouseY); // Create a fish at the mouse position
-  school.push(fish); // Add the fish to our array
-  // Now the school array has our new fish and it will be moved and drawn
-  // with all the others in the for loop!
+  // By passing the fortunes array as an argument to random() we get back
+  // a RANDOM ELEMENT in the array (one of the fortune strings) which we
+  // can then store in the chosenFortune variable for displaying
+  chosenFortune = random(fortunes);
 }
