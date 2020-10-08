530f5adc558ab609a72480f337834e7ce1e048eb
Pippin Barr <pippin.barr@gmail.com>
2020-10-08 12:01:59 -0400

    Arrays: singing dogs

--- sandbox/js/script.js
+++ sandbox/js/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let circle = {
-  x: 0,
-  y: 0,
-  size: 100,
-  trail: [],
-  maxTrail: 10
+let rates = [1.5, 1.75, 2.25, 2.5, 2.75, 3];
+
+let barkSFX;
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
 }
 
 // setup() the canvas ready
@@ -16,32 +16,9 @@ function setup() {
 // draw() draws a circle with a trails
 function draw() {
   background(0);
+}
 
-  // Use a for loop to go through each element in the circle's trail array in order
-  for (let i = 0; i < circle.trail.length; i++) {
-    // Get the element at the index indicated by i (0, then 1, then 2, etc.)
-    let element = circle.trail[i];
-    // Draw an ellipse the same size as the circle at that position
-    ellipse(element.x, element.y, circle.size);
-  }
-
-  // Move the circle to the mouse position
-  circle.x = mouseX;
-  circle.y = mouseY;
-
-  // Draw the circle
-  ellipse(circle.x, circle.y, circle.size);
-
-  // Create a new position object that stores where the circle is now
-  // which we can add to the trail to trace the path of the circle
-  let newTrailPosition = {
-    x: circle.x,
-    y: circle.y
-  };
-  // Add the position to the circle's trail array
-  circle.trail.push(newTrailPosition);
-
-  if (circle.trail.length > circle.maxTrail) {
-    circle.trail.shift();
-  }
+function mousePressed() {
+  barkSFX.rate(random(rates));
+  barkSFX.play();
 }
