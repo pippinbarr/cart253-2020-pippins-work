b9d199045efbeeca033ca82baf493e8cc1d4df95
Pippin Barr <pippin.barr@gmail.com>
2020-10-20 15:01:55 -0400

    Overlapping rectangles

--- sandbox/js/script.js
+++ sandbox/js/script.js
@@ -1,36 +1,62 @@
 "use strict";
 
-let balls = [];
-let numBalls = 3;
+// Two rectangles represented in terms of position, dimensions, and velocity
+let rect1 = {
+  x: 0,
+  y: 250,
+  width: 100,
+  height: 100,
+  vx: 2, // Moving right
+  vy: 0
+};
 
-let paddle;
-
-let gravity = 0.0025;
+let rect2 = {
+  x: 600,
+  y: 300,
+  width: 100,
+  height: 100,
+  vx: -2, // Moving left
+  vy: 0
+};
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  for (let i = 0; i < numBalls; i++) {
-    let ball = new Ball(random(0, width), random(-800, -100));
-    balls.push(ball);
-  }
-
-  paddle = new Paddle(200, 20);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background(0);
 
-  for (let i = 0; i < balls.length; i++) {
-    let ball = balls[i];
-    if (ball.active) {
-      ball.gravity(gravity);
-      ball.move();
-      ball.bounce(paddle);
-      ball.display();
-    }
+  // Move our rectangles according to their velocity
+  rect1.x = rect1.x + rect1.vx;
+  rect1.y = rect1.y + rect1.vy;
+
+  rect2.x = rect2.x + rect2.vx;
+  rect2.y = rect2.y + rect2.vy;
+
+  // We want to check if the rectangles overlap (collide) so let's
+  // track that in a variable. We'll assume they don't...
+  let overlap = false;
+
+  // Now we check if there is an overlap
+  if (rect1.x + rect1.width > rect2.x && // Is the right side of rect1 past the left side of rect2?
+    rect1.x < rect2.x + rect2.width && // Is the left side of rect1 NOT past the right side of rect2?
+    rect1.y + rect1.height > rect2.y && // Is the bottom of rect1 past the top of rect2?
+    rect1.y < rect2.y + rect2.height) { // Is the top of rect1 NOT past the bottom of rect2?
+    // If ALL of those things are true, the rectangles are overlapping.
+    overlap = true;
   }
 
-  paddle.move();
-  paddle.display()
+  noStroke();
+
+  // We fill the rectangles red if they overlap and green otherwise
+  if (overlap) {
+    fill(255, 0, 0);
+  }
+  else {
+    fill(0, 255, 0);
+  }
+
+  // Display our rectangles
+  rect(rect1.x, rect1.y, rect1.width, rect1.height);
+  rect(rect2.x, rect2.y, rect2.width, rect2.height);
 }
