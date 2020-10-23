b7051d2878f165b1e819297e540e100a0db29f33
Pippin Barr <pippin.barr@gmail.com>
2020-10-23 10:04:19 -0400

    OOP states example

--- sandbox/js/script.js
+++ sandbox/js/script.js
@@ -1,42 +1,39 @@
-"use strict";
+// A variable to store the currently active state object (starts empty)
+let currentState;
 
-// Our vehicles
-let vehicles = [];
-// How many of each kind
-let numCars = 5;
-let numSportsCars = 5;
-
-// Set up the canvas and our cars
+// setup()
+// Create the canvas, start our program in the title state, set default text style
 function setup() {
-  createCanvas(600, 600);
-  // Create the correct number of cars and put them in our vehicles array
-  for (let i = 0; i < numCars; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let car = new Car(x, y);
-    vehicles.push(car);
-  }
+  createCanvas(500, 500);
 
-  // Create the correct number of sports cars and put them in our vehicles array
-  for (let i = 0; i < numSportsCars; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let sportscar = new SportsCar(x, y);
-    vehicles.push(sportscar);
-  }
+  // We can set the current state by creating a NEW object from the class
+  // representing that state! This will call its constructor() which will work
+  // like the `setup()` for that state.
+  currentState = new Title();
+
+  // Text settings
+  textSize(32);
+  textAlign(CENTER, CENTER);
 }
 
-// Display and move the cars
+// draw()
+// Simply call the draw method of the current state
 function draw() {
-  background(0);
+  // If the current state is Title this will call the Title class draw()
+  // If the current state is Animation this will call the Animation class draw()
+  // if the current state is Ending this will call the Ending class draw()
+  currentState.draw();
+}
 
-  // Go through all the vehicles (cars and sportscars)
-  // and move, wrap, and display them. We can do this because we know
-  // that all vehicles have those methods!
-  for (let i = 0; i < vehicles.length; i++) {
-    let vehicle = vehicles[i];
-    vehicle.move();
-    vehicle.wrap();
-    vehicle.display();
-  }
+// keyPressed()
+// Call the keyPressed method of the current state
+// Note how even if the specific state itself DOESN'T define a keyPressed() method this
+// will work because they all extend the State class which does have one. For instance
+// neither Animation nor Ending define a keyPressed() method, but this still works
+// because they INHERIT the one from the State class.
+function keyPressed() {
+  // If the current state is Title this will call the Title class keyPressed()
+  // If the current state is Animation this will call the State class keyPressed()
+  // if the current state is Ending this will call the State class keyPressed()
+  currentState.keyPressed();
 }
