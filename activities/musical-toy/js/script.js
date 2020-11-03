59ab492b42badf0f2d04903984bf046858989568
Pippin Barr <pippin.barr@gmail.com>
2020-11-02 21:08:02 -0500

    Giant commit of sound experiments and activity

---
+++ activities/musical-toy/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**************************************************
+Musical Toy
+Pippin Barr
+
+A program that plays music based on primitive physics.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
